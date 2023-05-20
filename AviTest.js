b5a72b69887f47c9b6aecfe880dd5618ee606f1e
AvinashTSawant <atsawant92@gmail.com>
2023-05-20 11:42:46 +0530

    session11 for loop and counting  and ass.
    session11 for loop and counting  and ass.

--- AviTest.js
+++ AviTest.js
@@ -1,12 +1,35 @@
-function squareOfWordLength(num){
-  var as= num.length;
-  console.log(as);
-}
-squareOfWordLength("Avinash")
+var result = 0=='';
+console.log(result);
+console.log("-----------------------------------------");
+var result = 0=="0";
+console.log(result);
+console.log("-----------------------------------------");
+var result = 0==false;
+console.log(result);
+console.log("-----------------------------------------");
+var result = null==undefined;
+console.log(result);
+console.log("-----------------------------------------");
+var result = 1==[1];
+console.log(result);
+console.log("-----------------------------------------");
+var result = 1==true;
+console.log(result);
+console.log("-----------------------------------------");
+var result = 1=='1';
+console.log(result);
+console.log("-----------------------------------------");
+
+var num1 = 15;
+var result = num1%2 
+console.log(`${result}`);
+console.log("-----------------------------------------");
+
+ var num='100'
+var num1=100;
+var result=num + num1;
+console.log(result);
 
 
-function aa(num){
-  var as= num.length;
-  console.log(as)
-}
-aa("Avinash  ")
+
+console.log(0 == '0');
