869be9de15ccab52295836f0b6e6967ad76348c5
AvinashTSawant <atsawant92@gmail.com>
2023-05-30 11:14:20 +0530

    Session 17 clone,marge freezing  and ASS
    Session 17 clone,marge freezing  and ASS

---
+++ tt.js
@@ -0,0 +1,58 @@
+//   //simple traversal method
+//   let array = [6, 9, 15, 6, 13, 9, 11, 15]
+//   let index = 0, newArr = [];
+//   const length = array.length;
+//   for (let i = 0; i < length - 1; i++) {
+//    for (let j = i + 1; j < length; j++) {
+//    if (array[i] !== array[j]) {
+//       newArr[index] = array[i];
+//       }
+//    }
+ 
+//    index++;
+// }
+//  console.log(newArr);
+//======================================================
+  //simple traversal method
+//   let array = [6, 9, 15, 6, 13, 9, 11, 15, 15]
+//   let index = 0, newArr = [];
+//   const length = array.length;
+//   for (let i = 0; i < length - 1; i++) {
+//    for (let j = i + 1; j < length -1; j++) {
+//    if (array[i] == array[j]) {
+//       newArr[index] = array[i];
+//       index++;
+//       }
+//    }
+ 
+   
+// }
+//  console.log(newArr);
+
+ //================================================
+// const array = [1, 2, 4, 2, 5, 5, 5,5];
+// const duplicates = [];
+//  var index =0;
+// for (let i = 0; i < array.length-1; i++) {
+//   for (let j = i + 1; j < array.length-1; j++) {
+//     if (array[i] == array[j]) {
+//       duplicates[index]=array[i];
+//       index++;
+//     }
+//   }
+// }
+// console.log(duplicates)
+// //===============================================================
+
+// let arr = ['Avi', 'Sawi', 'Ats', 'Avi', 'Akm', 'Ats'];
+ 
+// let newArr = [];
+ 
+// for (let i = 0; i < arr.length; i++) {
+//     if (newArr.includes(arr[i]) === false) {
+//       newArr.push(arr[i]);
+//     }
+// }
+ 
+// console.log(newArr);
+//===============================================================
