c5b813ff463716028641483d77ac6a6a74040c3d
AvinashTSawant <atsawant92@gmail.com>
2023-06-02 11:56:59 +0530

    add mobile no array
    add mobile no array

--- 15_ObjectCloneAssign.js
+++ 15_ObjectCloneAssign.js
@@ -1,10 +1,8 @@
-console.log(
-  `---------------------shallow clone ----------------------------------------`,
-)
+console.log( `------------------------------------------------------------------------`,)
 const arrayNums = [20, 3, 4, 56, 90, 400, 49]
 console.log(`Given array is :- [${arrayNums}]`)
 let arrayClone = arrayNums
-console.log(`-------------------------------------------------------------`)
+console.log(`----------------------shallow clone---------------------------------------`)
 console.log(`1.Update cloned array with values→ 55, 66 using push`)
 arrayClone.push(55, 60)
 console.log(`1.A .Original array  After add element 55,60:- [${arrayNums}]`)
@@ -46,21 +44,26 @@ const employee_info = {
   mobiles: ['+91 8600 3456 88', '1800- 4567 32', '+91- 9096 5678 77'],
 }
 console.log( `--------------------------------------------------------------------`,)
-console.log(`4.Log the employee details `)
+console.log(`5.Log the employee details `)
 var addcolony =employee_info.address.locality.colony
 var addstreet =employee_info.address.locality.street
-console.log(`employee colony is  :- ${addcolony}`);
-console.log(`employee street is  :- ${addstreet}`);
+// console.log(`employee colony is  :- ${addcolony}`);
+// console.log(`employee street is  :- ${addstreet}`);
 
 var addcity =employee_info.address.city
 var addstate =employee_info.address.state
 var addcountry =employee_info.address.country
 
-console.log(`employee city is  :- ${addcity}`);
-console.log(`employee state  is  :- ${addstate}`);
-console.log(`employee country is  :- ${addcountry}`);
+// console.log(`employee city is  :- ${addcity}`);
+// console.log(`employee state  is  :- ${addstate}`);
+// console.log(`employee country is  :- ${addcountry}`);
+console.log(`-------------------------------------------------------------`)
+console.log(` 5.A Address :- ${addcolony},${addstreet},${addcity},${addstate},${addcountry}`);
+
+console.log(`-------------------------------------------------------------`)
+console.log(` 5.B Mobile no of employee is;- ${employee_info.mobiles}`);
 console.log( `--------------------------------------------------------------------`,)
-console.log(`5.deep copy using JSON.stringify() `)
+console.log(`6.deep copy using JSON.stringify() `)
 
 
 const deepClonedemployee_info = JSON.parse(JSON.stringify(employee_info));
@@ -70,14 +73,14 @@ employee_info.address.country= 'United Kingdom';
 
 
 console.log(`-----update 'july_month' salary to 80K in  cloned object-----`);
-console.log(`5.c Original object salary ; ${employee_info.salary.july_month} `);
+console.log(`6.c Original object salary ; ${employee_info.salary.july_month} `);
 
-console.log(`5.c cloned  object salary :- ${deepClonedemployee_info.salary.july_month}`);
+console.log(`6.c cloned  object salary :- ${deepClonedemployee_info.salary.july_month}`);
 
 
 console.log(`----- up date country= 'United Kingdom' in original object-----`);
 
 
-console.log(`5.c Original object country ; ${employee_info.address.country} `);
+console.log(`6.c Original object country ; ${employee_info.address.country} `);
 
-console.log(`5.c cloned  object country :- ${deepClonedemployee_info.address.country}`);
+console.log(`6.c cloned  object country :- ${deepClonedemployee_info.address.country}`);
