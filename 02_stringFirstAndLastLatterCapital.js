eeafac9ab3713bf723a7bda3e974852ad22b2fa9
AvinashTSawant <atsawant92@gmail.com>
2023-06-04 12:51:32 +0530

    Capitalize  1st and last latter of each word in String
    Capitalize 1st and last latter of each word in String

---
+++ 02_stringFirstAndLastLatterCapital.js
@@ -0,0 +1,18 @@
+var givenString = 'How are you mate'
+console.log(`Given string are :- "${givenString}"`)
+var sp = givenString.split('')
+// console.log(sp);
+for (let index = 0; index < sp.length; index++) {
+  const element = sp[index]
+  //  console.log(`${sp}`);
+
+  if (element == ' ') {
+    sp[index + 1] = sp[index + 1].toUpperCase()
+    sp[index - 1] = sp[index - 1].toUpperCase()
+    // console.log(sp[index-1]);
+    // console.log(sp[index+1]);
+  }
+}
+console.log(`String with 1st and last latter capital :- "${sp.join('')}"`)
+
+///////////////////////////////////////////////////////////////////////
